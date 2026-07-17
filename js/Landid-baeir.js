c620e3e4639fba89c8a1165b60382fee42c3af3c
BjossiJons <bjornjonsson9@gmail.com>
2026-07-17 12:06:34 +0000

    Fyrsta commit

---
+++ js/Landid-baeir.js
@@ -0,0 +1,52 @@
+const baeir = [
+  {
+    nafn: "Glaumbær",
+    lat: 65.6038,
+    lng: -19.5087,
+    myndir: [
+      {
+        src: "Myndir/Landid/Glaumbaer/glaumbaer-1.jpg",
+        texti: "Glaumbær séður frá suðri."
+      },
+      {
+        src: "Myndir/Landid/Glaumbaer/glaumbaer-2.jpg",
+        texti: "Burstabærinn í vetrarbúningi."
+      },
+      {
+        src: "Myndir/Landid/Glaumbaer/glaumbaer-3.jpg",
+        texti: ""
+      }
+    ],
+    texti: "Glaumbær í Skagafirði er einn þekktasti torfbær landsins og sýnir vel þróun torfbæja á Íslandi."
+  },
+
+  {
+    nafn: "Keldur",
+    lat: 63.8214,
+    lng: -20.0738,
+    myndir: [
+      {
+        src: "Myndir/Landid/Keldur/keldur-1.jpg",
+        texti: "Keldur á Rangárvöllum."
+      },
+      {
+        src: "Myndir/Landid/Keldur/keldur-2.jpg",
+        texti: ""
+      }
+    ],
+    texti: "Keldur á Rangárvöllum eru meðal merkustu varðveittu torfbæja landsins."
+  },
+
+  {
+    nafn: "Laufás",
+    lat: 65.8953,
+    lng: -18.0739,
+    myndir: [
+      {
+        src: "Myndir/Landid/Laufas/laufas-1.jpg",
+        texti: ""
+      }
+    ],
+    texti: "Laufás við Eyjafjörð er stór og vel þekktur torfbær sem sýnir búsetu og byggingarlag fyrri tíma."
+  }
+];
